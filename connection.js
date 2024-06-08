e48937f066cbeddd03397f5ac331d7db2f23b70b
Aditya Pachauri <103623274+AdityyaX@users.noreply.github.com>
2024-06-08 18:20:47 +0530

    url shortener updated

--- connection.js
+++ connection.js
@@ -1,10 +1,13 @@
 
 
 const mongoose = require('mongoose');
+require('dotenv').config();
+const mongoURI = process.env.MONGODB_URI;
+
 
 async function connect() {
   try {
-    await mongoose.connect('mongodb://localhost:27017/urlshortener');
+    await mongoose.connect(mongoURI);
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app=express();
 const bodyParser=require('body-parser');
 const router=require('./routes/url');
 const connect=require('./connection');
+require('dotenv').config();
+const port = process.env.PORT || 3000; 
 
 connect();
 app.use(bodyParser.json());
 app.use("/",router)
 
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log('Server is running on port 3000');
 })
